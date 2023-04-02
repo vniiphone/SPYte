1ccdc1478dc9a3882c39c9ac1e01218c8df7ab4a
Vu Nguyen <vuky12@icloud.com>
2023-04-03 01:49:15 +0700

    crud: product, category, brand
    Lỗi ở chổ thêm product, mặc dù đã tạo theo razor controller

---
+++ SPYte/Areas/Admin/Controllers/BrandsAdminController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SPYte.Data;
+using SPYte.Models;
+
+namespace SPYte.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandsAdminController : Controller
+    {
+        private readonly SPYteDBContext _context;
+
+        public BrandsAdminController(SPYteDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/BrandsAdmin
+        public async Task<IActionResult> Index()
+        {
+              return _context.Brands != null ? 
+                          View(await _context.Brands.ToListAsync()) :
+                          Problem("Entity set 'SPYteDBContext.Brands'  is null.");
+        }
+
+        // GET: Admin/BrandsAdmin/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Brands == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.Brands
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return View(brand);
+        }
+
+        // GET: Admin/BrandsAdmin/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/BrandsAdmin/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,CreatedDate,UpdatedDate,DeletedDate")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(brand);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(brand);
+        }
+
+        // GET: Admin/BrandsAdmin/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Brands == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.Brands.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: Admin/BrandsAdmin/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,CreatedDate,UpdatedDate,DeletedDate")] Brand brand)
+        {
+            if (id != brand.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    brand.UpdatedDate = DateTime.Now;
+                    _context.Update(brand);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BrandExists(brand.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(brand);
+        }
+
+        // GET: Admin/BrandsAdmin/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Brands == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.Brands
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return View(brand);
+        }
+
+        // POST: Admin/BrandsAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Brands == null)
+            {
+                return Problem("Entity set 'SPYteDBContext.Brands'  is null.");
+            }
+            var brand = await _context.Brands.FindAsync(id);
+            if (brand != null)
+            {
+                _context.Brands.Remove(brand);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BrandExists(int id)
+        {
+          return (_context.Brands?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ SPYte/Areas/Admin/Controllers/ProductsAdminController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SPYte.Data;
+using SPYte.Models;
+
+namespace SPYte.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsAdminController : Controller
+    {
+        private readonly SPYteDBContext _context;
+
+        public ProductsAdminController(SPYteDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/ProductsAdmin
+        public async Task<IActionResult> Index()
+        {
+            var sPYteDBContext = _context.Products.Include(p => p.Brand).Include(p => p.Cat);
+            return View(await sPYteDBContext.ToListAsync());
+        }
+
+        // GET: Admin/ProductsAdmin/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Brand)
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Name");
+            ViewData["CatId"] = new SelectList(_context.Categories, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Price,Stock,CreatedDate,UpdatedDate,DeletedDate,CatId,BrandId,Unit")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Name", product.BrandId);
+            ViewData["CatId"] = new SelectList(_context.Categories, "Id", "Name", product.CatId);
+            return View(product);
+        }
+        // GET: Admin/ProductsAdmin/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Name", product.BrandId);
+            ViewData["CatId"] = new SelectList(_context.Categories, "Id", "Name", product.CatId);
+            return View(product);
+        }
+
+        // POST: Admin/ProductsAdmin/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Name,Description,Price,Stock,CreatedDate,UpdatedDate,DeletedDate,CatId,BrandId,Unit")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Name", product.BrandId);
+            ViewData["CatId"] = new SelectList(_context.Categories, "Id", "Name", product.CatId);
+            return View(product);
+        }
+
+        // GET: Admin/ProductsAdmin/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Brand)
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Admin/ProductsAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Products == null)
+            {
+                return Problem("Entity set 'SPYteDBContext.Products'  is null.");
+            }
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(long id)
+        {
+          return (_context.Products?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
